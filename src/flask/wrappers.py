c3a3de12f976b7eacdebbae3683e8f407975814b
David Lord <davidism@gmail.com>
2023-04-12 13:43:23 -0700

    Merge pull request #5034 from adamtheturtle/default-mimetype-optional
    Allow Response subclasses to set default_mimetype as None

--- src/flask/wrappers.py
+++ src/flask/wrappers.py
@@ -149,7 +149,7 @@ class Response(ResponseBase):
         Added :attr:`max_cookie_size`.
     """
 
-    default_mimetype = "text/html"
+    default_mimetype: t.Optional[str] = "text/html"
 
     json_module = json
 
